3e9a80813fba72236a349ebc6f660f72e3a9a2c2
JeremyEllingham <jeremy@sethindustries.com.au>
2020-03-18 20:28:17 +0000

    Add second stream to get matches page.

--- client/src/components/matches/Match/index.js
+++ client/src/components/matches/Match/index.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../../actions';
+import flv from 'flv.js';
 
-const Match = () => {
+class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef1 = React.createRef();
+    this.videoRef2 = React.createRef();
+  }
+
+  componentDidMount() {
+    this.props.fetchStream(6);
+    this.props.fetchStream(7);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player1.destroy();
+    this.player2.destroy();
+  }
+
+  buildPlayer() {
+    if ((this.player1 && this.player2) || !this.props.stream1 || !this.props.stream2)
+      return;
+
+    this.player1 = flv.createPlayer({
+      type: 'flv',
+      url: `http://192.168.1.167:8000/live/6.flv`
+    });
+    if (!this.videoRef1.current) { return }
+    this.player1.attachMediaElement(this.videoRef1.current);
+    this.player1.load();
+
+    this.player2 = flv.createPlayer({
+      type: 'flv',
+      url: `http://192.168.1.167:8000/live/7.flv`
+    });
+    if (!this.videoRef2.current) { return }
+    this.player2.attachMediaElement(this.videoRef2.current);
+    this.player2.load();
+
+  }
+
+  render() {
+    if (!this.props.stream1 || !this.props.stream2 ) {
+      return <div>Loading...</div>
+    }
+
     return (
-        <div><p>Something</p></div>
+      <div>
+        <video ref={this.videoRef1} style={{ width: '40%' }} controls={true} />
+        <video ref={this.videoRef2} style={{ width: '40%' }} controls={true} />
+        <h1>{this.props.stream1.title}</h1>
+        <h5>{this.props.stream1.description}</h5>
+        <h1>{this.props.stream2.title}</h1>
+        <h5>{this.props.stream2.description}</h5>
+      </div>
     )
+  }
 }
 
-export default Match
+const mapStateToProps = (state, ownProps) => {
+  return { stream1: state.streams[6], stream2: state.streams[7] }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
