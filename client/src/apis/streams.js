7f472f1c0800b48596bbd0c19ce876bbc932cdda
JeremyEllingham <jeremy@sethindustries.com.au>
2020-03-19 20:37:43 +0000

    Update to use aws backend service.

--- client/src/apis/streams.js
+++ client/src/apis/streams.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://server:3002',
+    baseURL: 'http://13.54.200.18:3002',
     headers: {'Access-Control-Allow-Origin': '*'}
 });
